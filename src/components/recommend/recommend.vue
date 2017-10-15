dc1455ed51cbc45e179aa0278c5e8a15a8ed6723
= <134@126.com>
2017-10-15 20:16:35 +0800

    推荐轮播

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-        <div class="slider-wrapper"></div>
+        <div class="slider-wrapper" v-if="recommends.length">
+            <slider>
+                <!--填写插槽内容-->
+                <div  v-for="item in recommends">
+                    <a :href="item.linkUrl">
+                        <img :src="item.picUrl" alt=""/>
+                    </a>
+                </div>
+            </slider>
+        </div>
         <div class="recommend-list">
             <h1 class="list-title">热门歌单推荐</h1>
             <ul>
@@ -13,10 +22,16 @@
 </template>
 
 <script>
+  import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
+    data() {
+      return {
+          recommends:[]
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -24,11 +39,14 @@
       _getRecommend() {
           getRecommend().then((res) => {
               if(res.code === ERR_OK){
-                console.log(77,res.data.slider)
+                this.recommends = res.data.slider
               }
           })
       }
     },
+    components:{
+        Slider
+    }
   }
 </script>
 
